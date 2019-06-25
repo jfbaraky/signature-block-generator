fbe756afbabd6ed46d3ff754277c1ded85c023c0
@douglasabnovato <douglasabnovato@gmail.com>
2019-06-24 22:02:13 -0300

    atendendo aos requisitos do feedback pull request

--- src/components/Signature/Signature.js
+++ src/components/Signature/Signature.js
@@ -7,16 +7,9 @@ export default class Signature extends React.Component {
     this.state = {
       nomecompleto: "",
       cargoatual: "",
-      nomeempresa: "",
-      logoempresa: "",
-      avempresa: "",
-      salaempresa: "",
-      bairroempresa: "",
-      cidadeempresa: "",
-      numeroempresa: "",
-      telefoneempresa: "",
-      emailempresa: "",
-      redessociaisempresas: ""
+      telefone: "",
+      site: "",
+      cores: ""
     };
     this.onChange = (evento) => {
       const state = Object.assign({}, this.state);
@@ -28,13 +21,12 @@ export default class Signature extends React.Component {
       evento.preventDefault();
     };
   }
-
   render() {
     return (
       <div>
         <div id="inputs">
+          <h1>Preencha detalhes da sua assinatura :</h1>
           <div className="box">
-            <h1>Preencha detalhes da sua assinatura :</h1>
               <label> 
                 <span>Nome Completo</span> 
                 <input
@@ -54,90 +46,43 @@ export default class Signature extends React.Component {
                   onChange={this.onChange} />
               </label>
               <label>
-                <span>Nome Empresa </span>  
+                <span>Telefone </span>  
                 <input
-                  className="nomeempresa"
+                  className="telefone"
                   type="text"
-                  name="nomeempresa"
-                  value={this.state.nomeempresa}
+                  name="telefone"
+                  value={this.state.telefone}
                   onChange={this.onChange} />
               </label>
               <label>
-                <span>Logo Empresa</span>
+                <span>Site</span>
                 <input
-                  className="logoempresa"
+                  className="site"
                   type="text"
-                  name="logoempresa"
-                  value={this.state.logoempresa}
+                  name="site"
+                  value={this.state.site}
                   onChange={this.onChange} />
               </label>
-              <label><span>Av. Empresa </span>  
-                <input
-                  type="text"
-                  name="avempresa"
-                  value={this.state.avempresa}
-                  onChange={this.onChange} />
-              </label>
-              <label>
-                <span>Sala Empresa </span>
-                <input
-                  type="text"
-                  name="salaempresa"
-                  value={this.state.salaempresa}
-                  onChange={this.onChange} />
-              </label>
-              <label>bairro empresa : </label><input
-                type="text"
-                name="bairroempresa"
-                value={this.state.bairroempresa}
-                onChange={this.onChange} />
-              <label>cidade empresa : </label><input
-                type="text"
-                name="cidadeempresa"
-                value={this.state.cidadeempresa}
-                onChange={this.onChange} />
-              <label>número empresa : </label><input
-                type="text"
-                name="numeroempresa"
-                value={this.state.numeroempresa}
-                onChange={this.onChange} /> <br />
-              <label>telefone empresa : </label><input
-                type="text"
-                name="telefoneempresa"
-                value={this.state.telefoneempresa}
-                onChange={this.onChange} /> <br />
-              <label>e-mail empresa : </label><input
-                type="text"
-                name="emailempresa"
-                value={this.state.emailempresa}
-                onChange={this.onChange} />
-              <label>redes sociais empresas : </label><select name="redessociais" onChange={this.onChange} value={this.state.redessociais}>
+              <label>Cores : </label><select name="cores" onChange={this.onChange} value={this.state.cores}>
                 <option value="">Selecione</option>
-                <option value="1">Facebook</option>
-                <option value="2">Linkedin</option>
-                <option value="3">Twitter</option>
-                <option value="4">Google+</option>
-                <option value="5">Youtube</option>
+                <option value="1">Preto</option>
+                <option value="2">Azul</option>
+                <option value="3">Verde</option>
+                <option value="4">Amarelo</option>
+                <option value="5">Branco</option>
               </select><br />  
           </div>
         </div>
         <br />
-      
-
-
-        <div className="example">
-          <h1> {JSON.stringify(this.state.nomecompleto)}  </h1>
-          <h3> {JSON.stringify(this.state.cargoatual)}</h3>
-          <h4> {JSON.stringify(this.state.nomeempresa)},
-               {JSON.stringify(this.state.logoempresa)}</h4>
-          <h5> {JSON.stringify(this.state.avempresa)},
-               {JSON.stringify(this.state.salaempresa)}</h5>
-          <h5> {JSON.stringify(this.state.bairroempresa)}
-            {JSON.stringify(this.state.cidadeempresa)}
-            {JSON.stringify(this.state.numeroempresa)} </h5>
-          <h5> {JSON.stringify(this.state.telefoneempresa)}</h5>
-          <h5> {JSON.stringify(this.state.emailempresa)}
-            {JSON.stringify(this.state.redessociaisempresas)}</h5>
+        <div id="modelo">
+          <h1>Modelo da sua assinatura :</h1>
+          <div className="example">
+            <h2>{JSON.stringify(this.state.nomecompleto)} </h2>
+            <h3>{JSON.stringify(this.state.cargoatual)}</h3>
+            <h4>{JSON.stringify(this.state.telefone)}</h4>
+            <h5>{JSON.stringify(this.state.site)}</h5>
+            <h5>{JSON.stringify(this.state.cores)}</h5>
+          </div>
         </div>
         <br />
         <button onClick={this.onSubmit}>Enviar</button>
